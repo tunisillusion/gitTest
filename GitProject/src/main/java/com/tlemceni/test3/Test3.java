6179af53600168c255f87afe6360f34142ebc73d
ali007 <ali.tlemceni@esprit.tn>
2020-02-26 09:23:44 +0100

    adding two methods in test3

--- GitProject/src/main/java/com/tlemceni/test3/Test3.java
+++ GitProject/src/main/java/com/tlemceni/test3/Test3.java
@@ -2,7 +2,15 @@ package com.tlemceni.test3;
 
 public class Test3 {
 
-	public void test(){
+	public void test() {
+		System.out.println();
+	}
+	
+	void test1() {
+		System.out.println();
+	}
+	
+	void test2() {
 		System.out.println();
 	}
 
